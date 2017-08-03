f552f8ce6f5e1c8ea96c602bfedcaaaaa6a4aabb
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-08-03 09:00:53 +0200

    Functinality to add files to arrend.

--- source/php/Comments.php
+++ source/php/Comments.php
@@ -20,17 +20,17 @@ class Comments
         });
 
         //Acf hooks
-        add_action('acf/render_field/type=wysiwyg', array($this, 'listComments'), 15, 1);
+        add_action('acf/render_field/type=repeater', array($this, 'listComments'), 15, 1);
         add_action('acf/save_post', array($this, 'addComment'), 5, 1);
     }
 
     /**
-     * Sshow entered comments
+     * Show entered comments
      * @return void
      */
     public function listComments($field)
     {
-        if ($field['_name'] != "ticket_add_comment") {
+        if ($field['_name'] != "ticket_add_files") {
             return;
         }
 
@@ -41,6 +41,11 @@ class Comments
 
             if (is_array($previusComments) && !empty($previusComments)) {
                 foreach (array_reverse($previusComments) as $comment) {
+
+                    //Gather meta
+                    $filesMeta = get_comment_meta($comment->comment_ID, 'ticket_comment_files', true);
+
+                    //Start
                     $output .= '<table class="ticket-comments table widefat fixed" style="margin-top: 20px;">';
 
                     //Header
@@ -60,7 +65,23 @@ class Comments
                     $output .= '</td>';
                     $output .= '</tr>';
 
+                    //Files
+                    if (is_Array($filesMeta) && !empty($filesMeta)) {
+                        $output .= '<tr>';
+                        $output .= '<td colspan="2">';
+                        foreach ($filesMeta as $fileKey => $file) {
+                            if ($fileKey > 0) {
+                                $output .= ', ';
+                            }
+                            $output .= '<a target="_blank" href="' . wp_get_attachment_url($file) . '">' . get_the_title($file) . '</a>';
+                        }
+                        $output .= '</td>';
+                        $output .= '</tr>';
+                    }
+
                     $output .= '</tbody>';
+
+                    //End
                     $output .= '</table>';
                 }
             }
@@ -76,6 +97,9 @@ class Comments
     public function addComment($postId)
     {
 
+        //Definitions
+        $commentId = null;
+
         //Bail early if no ACF data
         if (empty($_POST['acf'])) {
             return;
@@ -83,7 +107,7 @@ class Comments
 
         //Add comment
         if (isset($_POST['acf']['field_598181d048f6a']) && !empty($_POST['acf']['field_598181d048f6a'])) {
-            $comment_id = wp_new_comment(array(
+            $commentId = wp_new_comment(array(
                 'comment_post_ID' => $postId,
                 'comment_author' => $this->currentUser->user_firstname . $this->currentUser->user_lastname,
                 'comment_author_email' => $this->currentUser->user_email,
@@ -92,7 +116,22 @@ class Comments
             ));
         }
 
-        //Reset value
-        $_POST['acf']['field_598181d048f6a'] = "";
+        //Append files to comment
+        if (!is_null($commentId) && is_numeric($commentId) && isset($_POST['acf']['field_5982c0ec0f328']) && !empty($_POST['acf']['field_5982c0ec0f328'])) {
+            $resultArray = array();
+            $dataTarget = $_POST['acf']['field_5982c0ec0f328'];
+
+            if (is_array($dataTarget) && !empty($dataTarget)) {
+                foreach ($dataTarget as $item) {
+                    $resultArray[] = array_pop($item);
+                }
+            }
+
+            add_comment_meta($commentId, 'ticket_comment_files', $resultArray);
+        }
+
+        //Reset value(s)
+        unset($_POST['acf']['field_598181d048f6a']);
+        unset($_POST['acf']['field_5982c0ec0f328']);
     }
 }
